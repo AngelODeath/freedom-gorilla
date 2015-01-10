eb45e80378eeec0d354c2f926cba353eec07725a
Gabri Botha <Angelodeath@outlook.com>
2015-01-10 12:31:09 +0200

    add index.js for sample code

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+                                                                                                                                                              
+router.get('/', function(req, res) {
+  res.render('index', { title: '+L=G10N+' });
+});
+
+module.exports = router;
+
+// exports.index = function(req, res){
+//   res.render('index', { title: 'Express' })
+// };
